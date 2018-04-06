1ca6bb24e02d459d2d820deddac2d1120311774c
nonceanfish <nonceanfish@gmail.com>
2018-04-05 23:59:17 -0400

    1

--- ann.py
+++ ann.py
@@ -1,12 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def getData():
+    df = pd.read_csv('./ecommerce_data.csv')
+    data = df.as_matrix()
+
+    X = data[:, :-1]
+    Y = data[:, -1]
+
+    X[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+    X[:, 2] = (X[:, 2] - X[:, 2].mean()) / X[:, 2].std()
+
+    N, D = X.shape
+    X2 = np.zeros((N, D+3))
+    X2[:, 0:(D-1)] = X[:, 0:(D-1)]
+
+    for n in range(N):
+        t = int(X[n, D-1])
+        X2[n, t+D-1] = 1
+
+    Z = np.zeros((N, 4))
+    Z[np.arange(N), X[:, D-1].astype(np.int32)] = 1
+    # X2[:, -4:] = Z
+    return X2, Y
+
+
+def get_binary_data():
+    X, Y = getData()
+    X2 = X[Y <= 1]
+    Y2 = Y[Y <= 1]
+    return X2, Y2
 
 
 def softmax(x):
-    expa = np.exp(x)
-    print('with:', expa / expa.sum(axis=1, keepdims=True))
-    print('without:', expa / expa.sum())
-    return expa / expa.sum()
+    expX = np.exp(x)
+    return expX / expX.sum(axis=1, keepdims=True)
 
 
 def sigmoid(x):
@@ -16,8 +46,8 @@ def sigmoid(x):
 def forward(x, w1, b1, w2, b2):
     t = x.dot(w1) + b1
     z = sigmoid(t)
-
-    return softmax(z.dot(w2) + b2)
+    s = z.dot(w2) + b2
+    return softmax(s)
 
 
 def classification_rate(Y, P):
