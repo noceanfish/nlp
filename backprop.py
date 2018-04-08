07ae98168c201b1ab48bbf02092ddab7f2e90f4c
nonceanfish <nonceanfish@gmail.com>
2018-04-08 18:03:11 -0400

    1

--- backprop.py
+++ backprop.py
@@ -32,35 +32,48 @@ def cross_cost(T, Y):
     return total.sum()
 
 
-def derivate_w2(Z, T, output):
+def derivative_w2(Z, T, Y):
     N, K = T.shape
     M = Z.shape[1]
 
-    ret1 = np.zeros((M, K))
-    for n in range(N):
-        for m in range(M):
-            for k in range(K):
-                ret1[m, k] += (T[n, k] - Y[n, k]) * Z[n, m]
-    return ret1
+    # ret1 = np.zeros((M, K))
+    # # for n in range(N):
+    # #     for m in range(M):
+    # #         for k in range(K):
+    # #             ret1[m, k] += (T[n, k] - Y[n, k]) * Z[n, m]
+    s = Z.T.dot(T - Y)
 
-def derivate_b2(T, Y):
+    return s
+
+
+def derivative_b2(T, Y):
     return (T - Y).sum(axis=0)
 
 
-def derivate_w1(X, Z, T, Y, W2):
+def derivative_w1(X, Z, T, Y, W2):
     N, D = X.shape
-    M. K = W2.shape
+    M, K = W2.shape
 
-    ret1 = np.zeros((D, M))
-    for n in range(N):
-        for k in range(K):
-            for m in range(M):
-                for d in range(D):
-                    ret1 += (T[n, k] - Y[n, k]) * W2[m, k] * Z[n, m] * (1 - Z[n, m]) * X[n, d]
-    return ret1
+    # ret1 = np.zeros((D, M))
+    # for n in range(N):
+    #     for k in range(K):
+    #         for m in range(M):
+    #             for d in range(D):
+    #                 ret1[d, m] += (T[n, k] - Y[n, k]) * W2[m, k] * Z[n, m] * (1 - Z[n, m]) * X[n, d]
+
+    # ret2 = np.zeros((D, M))
+    # for n in range(N):
+    #     for d in range(D):
+    #         for m in range(M):
+    #             for k in range(K):
+    #                 ret2[d, m] += (T[n, k] - Y[n, k]) * W2[m, k] * Z[n, m] * (1 - Z[n, m]) * X[n, d]
+    #
+    # print("ret1:", ret1, "ret2:", ret2)
+    dZ = (T - Y).dot(W2.T)*Z*(1-Z)
+    return X.T.dot(dZ)
 
 
-def derivate_b1(T, Y, W2, Z):
+def derivative_b1(T, Y, W2, Z):
     return ((T - Y).dot(W2.T) * Z * (1 - Z)).sum(axis=0)
 
 
@@ -96,7 +109,7 @@ def main():
             c = cross_cost(T, output)
             p = np.argmax(output, axis=1)
             r = classification_rate(Y, p)
-            print("cost:", c, "cloassifition_rate:", r)
+            print("", epoch, "cost:", c, "cloassifition_rate:", r)
             costs.append(c)
 
         W2 += learning_rate*derivative_w2(Z, T, output)
@@ -106,7 +119,6 @@ def main():
 
     plt.plot(costs)
     plt.show()
-
 
 
 if __name__ == "__main__":
